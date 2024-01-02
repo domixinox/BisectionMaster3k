57c3e93d3915b410f443e52b5a7acbd67ce1afc7
PanKlak <152812549+PanKlak@users.noreply.github.com>
2024-01-02 15:07:11 +0100

    Parser (Very Beta)

---
+++ BisectionMaster3k/Parser.cs
@@ -0,0 +1,109 @@
+﻿using BisectionMaster3k.Datatypes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisectionMaster3k
+{
+
+
+
+
+
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    /**
+     * Klasa Parser - Cele:
+     * #Parsuje tekst
+     */
+    static class Parser
+    {
+        
+
+
+
+
+
+
+       static public void LeParse(string text,string a)
+        {
+
+            for (int i = 0; i < text.Length; i++)// czyta cały tekst literka po literce
+            {
+                if (text[i] == '^')  // jeżeli jest potęga (poniżej wyjaśnie dlaczego)
+                {
+                    int pwn = i;
+                    for
+
+                    int power = text[i+1]-'0';
+                       
+                    text = text.Remove(i, 1);
+                    text = text.Remove(i, 1);
+
+                    for (int g = 1; g < power; g++)
+                    {
+                        text = text.Insert(i,"*x");
+                    }
+
+
+
+                }
+            }
+            for (int i = 0; i < text.Length; i++)// Znowu czyta cały tekst literka po literce
+            {
+                if (text[i] == 'x') //jeżeli jest x to wymien na string "a" podany wczesniej w fukcji LeParse
+                {
+
+                    text = text.Remove(i, 1);
+                    text = text.Insert(i, a);
+
+
+
+                }
+
+            }
+
+
+            DataTable dt = new DataTable();
+            var test = dt.Compute(text, ""); //Compute Nie może potęgować wieć zamieniłem cały process 
+
+
+
+            
+
+            MessageBox.Show(test.ToString());
+
+
+
+        }
+
+        static public void LeParse2(string text)
+        {
+           
+               
+                    string[] subs = text.Split("x", '\t');
+            foreach (var sub in subs)
+            {
+                MessageBox.Show($"Substring: {sub}");
+                
+                //Coefficients.add({sub });
+            }
+            
+                 
+                  
+                   
+              
+
+
+
+
+        }
+
+
+
+    }
+}
